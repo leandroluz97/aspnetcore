3fcf60d1f5b0ec9d1ee8b6304a44c81d8e35c406
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-20 23:47:56 +0000

    added Evironment examples

---
+++ EnviromentsExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnviromentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWebHostEnvironment _webHostEnv;
+        public HomeController(IWebHostEnvironment webHostEnv)
+        {
+            _webHostEnv = webHostEnv;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //_webHostEnv.IsDevelopment();
+            ViewBag.CurrentEnviroment = _webHostEnv.EnvironmentName;
+            return View();
+        }
+    }
+}
