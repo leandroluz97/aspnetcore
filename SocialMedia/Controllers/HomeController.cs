898870b5e1161af84be64c103ea14ebf3f44c87a
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-21 23:05:13 +0000

    Added Social media folder structure

---
+++ SocialMedia/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.Facebook = _configuration["SocialMediaLinks:Facebook"];
+            ViewBag.Instagram = _configuration["SocialMediaLinks:Instagram"];
+            ViewBag.Twitter = _configuration["SocialMediaLinks:Twitter"];
+            ViewBag.Youtube = _configuration["SocialMediaLinks:Youtube"];
+
+            return View();
+        }
+    }
+}
---
+++ SocialMedia/Models/Social.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMedia.Models
+{
+    public class Social
+    {
+        public string? Facebook { get; set; }
+        public string? Instagram { get; set; }
+        public string? Twitter { get; set; }
+        public string? Youtube { get; set; }
+    }
+}
