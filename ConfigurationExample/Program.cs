c5b05ab8694d6e1ab9cfa7f2f9d565b81a75ad4a
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-21 23:04:20 +0000

    Added ConfigurationExample chapter

--- ConfigurationExample/Program.cs
+++ ConfigurationExample/Program.cs
@@ -1,11 +1,24 @@
+//* Used for development
+//dotnet user-secrets init
+//dotnet user-secrets set "weatherapi:ClientID" "ClientID from client secrets"
+//* Used for production
+//Used for development
+//$Env: weatherapi__ClientID = "Client from environment variables"
 using ConfigurationExample;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
 //Supply an object of weatherApiOption  as a Service
 builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherApi"));
-var app = builder.Build();
+// Load MyOwnConfigFile.json
+builder.Host.ConfigureAppConfiguration((hostingContext, config) => 
+{
+    config.AddJsonFile("MuOwnConfigFile", optional:true, reloadOnChange:true );
+});
 
+
+var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();   
 app.MapControllers();
