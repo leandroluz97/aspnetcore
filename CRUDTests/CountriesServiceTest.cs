1964329a6aa3ceee67137e5ac5af1685d3ebfe8e
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-24 00:56:15 +0000

    Added countriesService unit tests

--- CRUDTests/CountriesServiceTest.cs
+++ CRUDTests/CountriesServiceTest.cs
@@ -1,19 +1,91 @@
-﻿using ServicesContracts;
+﻿using CRUDoperations.DTO;
+using ServicesContracts;
 using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XServices;
+using Xunit;
 
 namespace CRUDTests
 {
-    internal class CountriesServiceTest
+    public class CountriesServiceTest
     {
         private readonly IContriesService _countriesServices;
-
         public CountriesServiceTest()
         {
-
+            _countriesServices = new CountriesService();
         }
+
+        [Fact]
+        public void ExpectValidResponse()
+        {
+            //Arrange
+            CountryAddRequest request = new CountryAddRequest() { CountryName = "Leandro" };
+            CountryResponse response = new CountryResponse() { CountryName= "Leandro", CountryId = new Guid("20d5495a-1995-4f9e-827d-a99494d25cfa") };
+            
+            //Act
+            var actual = _countriesServices.AddCountry(request);
+
+            //Assert
+            //Assert.Equal(response, actual);
+            var obj1Str = JsonConvert.SerializeObject(response);
+            var obj2Str = JsonConvert.SerializeObject(actual);
+            Assert.Equal(obj1Str, obj2Str);
+        }
+
+        [Fact]
+        public void ExpectCountryAddRequestArgumentNullException()
+        {
+            try
+            {
+                //Arrange
+                CountryResponse response = new CountryResponse() { CountryName = "Leandro", CountryId = Guid.NewGuid() };
+                //Act
+                var actual = _countriesServices.AddCountry(null);
+            }
+            catch (ArgumentNullException exception)
+            {
+                //Assert
+                Assert.IsType<ArgumentNullException>(exception);
+            } 
+        }
+
+        [Fact]
+        public void ExpectCountryAddRequestNameArgumentNullException()
+        {
+            try
+            {
+                //Arrange
+                CountryAddRequest request = new CountryAddRequest() { CountryName = null };
+                //Act
+                var actual = _countriesServices.AddCountry(request);
+            }
+            catch (ArgumentNullException exception)
+            {
+                //Assert
+                Assert.IsType<ArgumentNullException>(exception);
+            }
+        }
+
+        [Fact]
+        public void ExpectSameNameToThrowArgumentNullException()
+        {
+            try
+            {
+                //Arrange
+                CountryAddRequest request = new CountryAddRequest() { CountryName ="Caboverde" };
+                //Act
+                var actual = _countriesServices.AddCountry(null);
+            }
+            catch (ArgumentNullException exception)
+            {
+                //Assert
+                Assert.IsType<ArgumentNullException>(exception);
+            }
+        }
+
     }
 }
--- XServices/CountriesService.cs
+++ XServices/CountriesService.cs
@@ -1,13 +1,37 @@
 ﻿using CRUDoperations.DTO;
+using Entities;
 using ServicesContracts;
 
 namespace XServices
 {
     public class CountriesService : IContriesService
     {
+        List<Country> _countries;
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+            _countries.Add(new Country() { CountryId = Guid.NewGuid(), CountryName = "Caboverde" });
+            _countries.Add(new Country() { CountryId = Guid.NewGuid(), CountryName = "Portugal" });
+            _countries.Add(new Country() { CountryId = Guid.NewGuid(), CountryName = "Brazil" });
+
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            if(countryAddRequest == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (string.IsNullOrEmpty(countryAddRequest.CountryName))
+            {
+                throw new ArgumentNullException();
+            }
+            if(_countries.Find(c => c.CountryName == countryAddRequest.CountryName) != null) 
+            {
+                throw new ArgumentNullException();
+            }
+            Country country = new Country() { CountryId = new Guid("20d5495a-1995-4f9e-827d-a99494d25cfa"), CountryName = countryAddRequest.CountryName };
+            return country.ToCountryResponse();
         }
     }
 }
