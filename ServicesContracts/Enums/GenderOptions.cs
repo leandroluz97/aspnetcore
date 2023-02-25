d602e0f22a906fd3fa077f179b7552a518f2c69f
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-25 18:28:56 +0000

    Added GenderOption enum

---
+++ ServicesContracts/Enums/GenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.Enums
+{
+    public enum GenderOptions
+    {
+        Male, Female, Other
+    }
+}
