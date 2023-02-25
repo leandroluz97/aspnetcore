3f69552425adc586fee8f925932231b74124f8f3
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-25 18:28:41 +0000

    Added PersonAddRequest DTO model

---
+++ ServicesContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using ServicesContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.DTO
+{
+    public class PersonAddRequest
+    { 
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryId { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+    }
+}
