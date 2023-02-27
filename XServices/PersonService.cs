42227c7fc0f5fdf5b5e3ad48561fc51291b1ce34
leandroluz97 <leandrodaluz97@gmail.com>
2023-02-27 00:10:29 +0000

    Added personServices with IPersonService contracts

---
+++ XServices/PersonService.cs
@@ -0,0 +1,23 @@
+﻿using ServicesContracts;
+using ServicesContracts.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XServices
+{
+    public class PersonService : IPersonService
+    {
+        public PersonResponse AddPerson(PersonAddRequest personRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PersonResponse> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
