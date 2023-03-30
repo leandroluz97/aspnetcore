50e69eda8428c697918d5640aa7db8c3b825c042
leandroluz97 <leandrodaluz97@gmail.com>
2023-03-31 00:33:07 +0100

    added rotatitiva package to generate pdfs

--- CRUDoperations/Controllers/PersonController.cs
+++ CRUDoperations/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
 using ServicesContracts;
 using ServicesContracts.DTO;
 using ServicesContracts.Enums;
@@ -8,17 +9,17 @@ namespace CRUDoperations.Controllers
     public class PersonController : Controller
     {
         private readonly IPersonService _personsService;
-        private readonly ICountriesService _countriesService;   
-        public PersonController(IPersonService personService, ICountriesService  countriesService)
+        private readonly ICountriesService _countriesService;
+        public PersonController(IPersonService personService, ICountriesService countriesService)
         {
             _personsService = personService;
             _countriesService = countriesService;
         }
-        
+
 
         [Route("persons/index")]
         [Route("/")]
-        public async Task<IActionResult> Index(string searchBy, string? searchText, string sortBy = "PersonName", SortOrderOptions sortOrder = SortOrderOptions.ASC )
+        public async Task<IActionResult> Index(string searchBy, string? searchText, string sortBy = "PersonName", SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
             //Searching
             ViewBag.SearchFields = new Dictionary<string, string>()
@@ -46,9 +47,9 @@ namespace CRUDoperations.Controllers
 
         [Route("persons/create")]
         [HttpGet]
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            var countries = _countriesService.GetAllCountries();
+            var countries = await _countriesService.GetAllCountries();
             ViewBag.Countries = countries;
             return View();
         }
@@ -60,13 +61,24 @@ namespace CRUDoperations.Controllers
             if (!ModelState.IsValid)
             {
                 var countries = _countriesService.GetAllCountries();
-                ViewBag.Countries = countries;   
+                ViewBag.Countries = countries;
                 ViewBag.Errors = ModelState.Values.SelectMany(err => err.Errors).Select(err => err.ErrorMessage).ToList();
                 return View();
             }
 
             _personsService.AddPerson(personAddRequest);
             return RedirectToAction("Index", "Person");
+        }
+
+        [Route("persons/PersonsPDF")]
+        public async Task<IActionResult> PersonsPDF()
+        {
+            var persons = await _personsService.GetAllPersons();
+            return new ViewAsPdf("PersonsPDF", persons, ViewData)
+            {
+                PageMargins = new Rotativa.AspNetCore.Options.Margins { Top = 20, Right = 20, Bottom = 20, Left = 20 },
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Landscape
+            };
         }
     }
 }
