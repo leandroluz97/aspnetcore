a1387663e1d64653447837ca197adf1381cde117
leandroluz97 <leandrodaluz97@gmail.com>
2023-04-08 23:04:56 +0100

    Set up personsController integration test

---
+++ CRUDTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDTests
+{
+    public class PersonsControllerIntegrationTest
+    {
+        
+        #region Index
+        public void Index_ToReturnView()
+        {
+            //Arrange
+
+            //Act
+            HttpResponseMessage response = _client.GetAsync("/Persons/Index");
+
+            //Assert
+            response.Should().BeSuccessful();
+        }
+        #endregion
+    }
+}
