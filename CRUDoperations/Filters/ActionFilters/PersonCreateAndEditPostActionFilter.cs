c8a6f5f6224e99f09598462c28d9fbee5f29e25e
leandroluz97 <leandrodaluz97@gmail.com>
2023-04-15 11:39:22 +0100

    implemented actions filters

---
+++ CRUDoperations/Filters/ActionFilters/PersonCreateAndEditPostActionFilter.cs
@@ -0,0 +1,36 @@
+﻿using CRUDoperations.Controllers;
+using CRUDoperations.DTO;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServicesContracts;
+
+namespace CRUDoperations.Filters.ActionFilters
+{
+    public class PersonCreateAndEditPostActionFilter : IAsyncActionFilter
+    {
+        private readonly ICountriesService _countriesService;
+        public PersonCreateAndEditPostActionFilter(ICountriesService countriesService)
+        {
+            _countriesService = countriesService;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //Todo : Before logic
+            if(context.Controller is PersonController personsController)
+            {
+                if (!personsController.ModelState.IsValid)
+                {
+                    List<CountryResponse> countries = await _countriesService.GetAllCountries();
+                    personsController.ViewBag.Countries = countries;
+                    personsController.ViewBag.Errors = personsController.ModelState.Values.SelectMany(err => err.Errors).Select(err => err.ErrorMessage).ToList();
+                    var personRequest = context.ActionArguments["personRequest"];
+                    context.Result = personsController.View(personRequest);  //short-circuits or skips the subsequent action filter & action method
+                }
+            }
+            else
+            {
+                await next();
+            }
+            //Todo : After logic
+        }
+    }
+}
